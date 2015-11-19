b0d54ffcc9aa7c8f8c5669b00515763170a15218
flinus <flinus12@gmail.com>
2015-11-20 08:54:38 +0900

    컨트롤러 누락 파일 커

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good morning" : "Good afternoon";
+
+            return View();
+        }
+
+        //
+        // GET: /Home/RsvpForm
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        //
+        // POST : /Home/RsvpForm
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            // TODO: 파티 주쵲에게 guestResponse를 전자우편으로 발송한다.
+            return View("Thanks", guestResponse);
+        }
+    }
+}
