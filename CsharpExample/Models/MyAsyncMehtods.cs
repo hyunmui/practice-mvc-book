d85d7117a1f51e20eacceb84b4958143b6b24e18
Hyeon-min Seon <hyeonmin.seon@nzrtbs.com>
2016-06-15 08:03:32 +0900

    async & await

---
+++ CsharpExample/Models/MyAsyncMehtods.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CsharpExample.Models
+{
+    public class MyAsyncMehtods
+    {
+        public static Task<long?> GetPageLength()
+        {
+            HttpClient client = new HttpClient();
+            var httpTask = client.GetAsync("http://apress.com");
+
+            return httpTask.ContinueWith((Task<HttpResponseMessage> antecedent) =>
+            {
+                return antecedent.Result.Content.Headers.ContentLength;
+            });
+        }
+
+        public async static Task<long?> GetPageLengthByAsync()
+        {
+            HttpClient client = new HttpClient();
+            var httpMesage = await client.GetAsync("http://apress.com");
+
+            return httpMesage.Content.Headers.ContentLength;
+        }
+    }
+}
