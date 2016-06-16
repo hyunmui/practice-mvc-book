f6ce8c088af6fe649eb3198909ec326cfb0e110d
Hyeon-min Seon <hyeonmin.seon@nzrtbs.com>
2016-06-17 08:45:45 +0900

    First Ninject use

--- EssentialTools/EssentialTools/Controllers/HomeController.cs
+++ EssentialTools/EssentialTools/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ namespace EssentialTools.Controllers
 {
     public class HomeController : Controller
     {
+        private IValueCalculator calc;
         private Product[] products =  
                 {
                     new Product { Name="Kayak", Category = "Watersports", Price = 275M },
@@ -14,18 +15,14 @@ namespace EssentialTools.Controllers
                     new Product { Name="Corner flag", Category = "Soccer", Price = 34.95M },
                 };
 
+        public HomeController(IValueCalculator calcParam)
+        {
+            calc = calcParam;
+        }
+
         // GET: Home
         public ActionResult Index()
         {
-            IKernel ninjectKernel = new StandardKernel();
-            ninjectKernel.Bind<IValueCalculator>().To<LinqValueCalculator>();       // 이부분이 의존성이 남아있음
-
-            // 의존성 남아있음
-            //IValueCalculator calc = new LinqValueCalculator();
-
-            // NInject를 통해 의존성 제거
-            IValueCalculator calc = ninjectKernel.Get<IValueCalculator>();
-
             ShoppingCart cart = new ShoppingCart(calc) { Products = products };
 
             decimal totalValue = cart.CalculateProductTotal();
