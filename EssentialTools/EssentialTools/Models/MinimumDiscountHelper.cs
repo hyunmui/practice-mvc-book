4050e57997d9e11713f4f2a0d9f59a2dd977a5cc
Hyeon-min Seon <hyeonmin.seon@nzrtbs.com>
2016-06-20 07:35:07 +0900

    make complete unittest module

---
+++ EssentialTools/EssentialTools/Models/MinimumDiscountHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EssentialTools.Models
+{
+    /// <summary>
+    ///  클래스 구현 목표
+    ///  1. 합계가 $100을 초과하면 10% 할인율 적용
+    ///  2. 합계가 $10에서 $100 사이면 $5의 할인율 적용
+    ///  3. 합계가 $10 미만이면 할인율 미적용
+    ///  4. 합계가 $0보다 작으면 ArgumentOutOfRangeException 예외 발생
+    /// </summary>
+    public class MinimumDiscountHelper : IDiscountHelper
+    {
+        public decimal ApplyDiscount(decimal totalParam)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
