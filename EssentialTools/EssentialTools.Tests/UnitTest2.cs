0abbfcce5ba1a5f4f77fbe14c493a181609c8665
Hyeon-min Seon <hyeonmin.seon@nzrtbs.com>
2016-06-21 07:15:05 +0900

    Simple Moq Example

--- EssentialTools/EssentialTools.Tests/UnitTest2.cs
+++ EssentialTools/EssentialTools.Tests/UnitTest2.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using EssentialTools.Models;
 using System.Linq;
+using Moq;
 
 namespace EssentialTools.Tests
 {
@@ -20,15 +21,16 @@ namespace EssentialTools.Tests
         public void Sum_Products_Correctly()
         {
             // Arrange
-            var discounter = new MinimumDiscountHelper();
-            var target = new LinqValueCalculator(discounter);
-            var goalTotal = products.Sum(e => e.Price);
+            Mock<IDiscountHelper> mock = new Mock<IDiscountHelper>();
+            mock.Setup(m => m.ApplyDiscount(It.IsAny<decimal>()))
+                .Returns<decimal>(total => total);
+            var target = new LinqValueCalculator(mock.Object);
 
             // Act
             var result = target.ValueProducts(products);
 
             // Assert
-            Assert.AreEqual(goalTotal, result);
+            Assert.AreEqual(products.Sum(e => e.Price), result);
         }
     }
 }
